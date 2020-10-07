5bad3141fb5ed92efaa9a277c080b793d1db8ae5
rahulgott <rahul.gott@gmail.com>
2020-10-07 11:18:39 +0530

    Updates

---
+++ precache-manifest.cff19119e5451de2ea916ed346ffe031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2fba917c28c71e316dfff982997727",
+    "url": "/Keeper-FrontEnd/index.html"
+  },
+  {
+    "revision": "bd236cccd3ef1a4c9bcf",
+    "url": "/Keeper-FrontEnd/static/js/2.4e4adf0d.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/Keeper-FrontEnd/static/js/2.4e4adf0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88845adbf89b02eb607",
+    "url": "/Keeper-FrontEnd/static/js/main.bfdc2df4.chunk.js"
+  },
+  {
+    "revision": "86b60c61b49cfe375f11",
+    "url": "/Keeper-FrontEnd/static/js/runtime-main.2f15db81.js"
+  }
+]);
